64e14467aa1c7bf2641ad52108e244fbed13389a
evilbabaroga <kiko_lazevski@yahoo.com>
2023-02-06 18:19:26 +0100

    Redesign, add queries, table view, alerts

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import json
 import os
-from flask import Flask, render_template, jsonify, send_from_directory
+from flask import Flask, render_template, send_from_directory, redirect, url_for, request
 from src.db_utils import Database
 from src.mongo_db_utils import MongoDatabase
 from src.utils import clean_company_name
 from flask_cors import CORS
+from src.utils import replace_all
 
 app = Flask(__name__)
 CORS(app)
@@ -14,27 +15,53 @@ DB_NAME = "semos_companies_data.db"
 TABLE = "companies"
 MONGO_STR = "mongodb://localhost:27017"
 MONGO_KEY = "company_name_cleaned"
-
 db = Database(DB_NAME)
-@app.route("/")
+view = "json"
+
+def create_mongo_db(start=1, end=20):
+    try:
+        mongo_db = MongoDatabase(db, MONGO_STR, "semos_companies", {TABLE: MONGO_KEY})
+        entries = mongo_db.read_db(TABLE, start, end)
+        numdocs = replace_all(",", ".", format(mongo_db.num_docs(TABLE), ","))
+        mongo_db_key = mongo_db.keys[TABLE]
+        columns = mongo_db.columns[TABLE]
+        tableEntries = [['MongoDB key'] + columns]
+        rows = [[list(entry.keys())[1]] + list(entry[list(entry.keys())[1]].values()) for entry in entries]
+        tableEntries.extend(rows)
+        for entry in entries:
+            entry['_id'] = str(entry['_id'])
+    except ValueError:
+        return render_template(HTML_FILE, error=True)
+    return render_template(HTML_FILE, dbName=mongo_db.name, tableName=TABLE, numdocs=numdocs, mongo_db_key=mongo_db_key, entries=[json.dumps(entry, indent=4) for entry in entries], tableEntries=tableEntries, loaded=True)
+
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-    return render_template(HTML_FILE, dbName='', tableName='')
+    if request.method == "POST":
+        db.update_columns(TABLE, MONGO_KEY, value_pairs=db.get_column_by_pk(TABLE, "name", clean_company_name))
+        return render_template(HTML_FILE, dbName="-- DB not loaded --", tableName="-- DB not loaded --", cleaned="True")
+    return render_template(HTML_FILE, dbName="-- DB not loaded --", tableName="-- DB not loaded --")
 
-@app.route("/cleanDB")
-def cleanDB():
-    db.update_columns(TABLE, MONGO_KEY, value_pairs=db.get_column_by_pk(TABLE, "name", clean_company_name))
-    return render_template(HTML_FILE, dbName='', tableName='')
+# @app.route("/cleanDB")
+# def cleanDB():
+    # db.update_columns(TABLE, MONGO_KEY, value_pairs=db.get_column_by_pk(TABLE, "name", clean_company_name))
+    # return redirect(url_for("/"))
 
-@app.route("/mongoDBRead")
+@app.route("/mongoDBRead/", methods=['GET', 'POST'])
 def mongoDBRead():
-    mongo_db = MongoDatabase(db, MONGO_STR, "semos_companies", {TABLE: MONGO_KEY})
-    entries = mongo_db.read_db(TABLE, 20)
-    for entry in entries:
-        entry['_id'] = str(entry['_id'])
-    # for entry in entries:
-        # del entry['_id']
-    return render_template(HTML_FILE, dbName=mongo_db.name, tableName=TABLE, entries=[json.dumps(entry, indent=4) for entry in entries])
-        # print('\n'.join(map(str, mongo_db.read_db(TABLE, 20))))
+    if request.method == "POST":
+        try:
+            start = request.form["from"]
+            end = request.form["to"]
+        except Exception:
+            return hello()
+        try:
+            start = int(start)
+            end = int(end)
+            return create_mongo_db(start, end)
+        except Exception:
+            return create_mongo_db(0, 0)
+    return create_mongo_db()
+
 
 @app.route('/favicon.ico')
 def favicon():
--- src/mongo_db_utils.py
+++ src/mongo_db_utils.py
@@ -1,6 +1,7 @@
 from src.db_utils import Database
 import pymongo
 from cryptography.fernet import Fernet
+from bson.objectid import ObjectId
 import json
 
 # password = "ASDFGHJKLR$9a-fuj20,f-./d=-.2,2E$D%"
@@ -23,6 +24,8 @@ class MongoDatabase:
         self.name = name
         self.tables = sqlite_database.tables
         self.primary_keys = sqlite_database.primary_keys
+        self.keys = dict()
+        self.columns = dict()
         client = pymongo.MongoClient(mongo_str, serverSelectionTimeoutMS=5000)
         try:
             client.server_info()
@@ -31,24 +34,29 @@ class MongoDatabase:
             print(e)
             return
         self.mongo_db = client[self.name]
-        if self.mongo_db.name in client.list_database_names():
-            print("[+] MongoDB initialized")
-            return
         with open("crypto_key.txt", "rb") as file_key:
             crypto_key = file_key.read()
         fernet = Fernet(crypto_key)
         for table in self.tables:
+            key = keys[table]
+            if type(key) is not str:
+                print("[x] Invalid MongoDB key value")
+                raise ValueError
+            self.keys[table] = key
             documents = dict()
             table_collection = self.mongo_db[table]
             data_chunks = sqlite_database.read_db(table, chunk_size=2000)
             columns = sqlite_database.columns(table)
-            key = keys[table]
+            self.columns[table] = [column for column in columns if column != key]
+            if self.mongo_db.name in client.list_database_names():
+                continue
             for data_chunk in data_chunks:
                 for row in data_chunk:
                     entry = {pair[0]:pair[1] for pair in zip(columns, row)}
                     curr_key = entry[key]
+                    if curr_key is None:
+                        raise ValueError
                     del entry[key]
-                    print(curr_key, type(curr_key))
                     entry_no_id = {val for val in entry if val[0] != "id"}
                     if curr_key in documents.keys():
                         if entry_no_id not in documents[curr_key]:
@@ -63,7 +71,7 @@ class MongoDatabase:
                         table_collection.insert_one({encrypted_key:encrypted_val})
         print("[+] MongoDB initialized")
 
-    def read_db(self, table, num):
+    def read_db(self, table, start, end):
         """
         Read a given number of elements successively from the Mongo database.
 
@@ -71,17 +79,30 @@ class MongoDatabase:
             table: str - name of the table
             num: int - number of elements to read
         """
+        first_id = self.objectid_to_id(self.mongo_db[table].find_one()['_id'])
+        direction = 1 if start < end else -1
         with open("crypto_key.txt", "rb") as file_key:
             crypto_key = file_key.read()
         fernet = Fernet(crypto_key)
         entries = []
-        cursor = self.mongo_db[table].find()
-        i = 0
-        for documnet in cursor:
-            if i == num:
+        start_obj_id = ObjectId(hex(start + first_id - 1)[2:]) 
+        end_obj_id = ObjectId(hex(end + first_id - 1)[2:]) 
+        
+        num = max([abs(end - start) + 1, 1])
+
+        if start < end:
+            cursor = self.mongo_db[table].find({'_id': {"$gte": start_obj_id, "$lte": end_obj_id}}).sort('_id', direction).batch_size(num)
+        else:
+            cursor = self.mongo_db[table].find({'_id': {"$lte": start_obj_id, "$gte": end_obj_id}}).sort('_id', direction).batch_size(num)
+
+        for _ in range(num):
+            try:
+                document = cursor.next()
+            except StopIteration:
+                print('error iter')
                 break
             curr = dict()
-            for key, val in documnet.items():
+            for key, val in document.items():
                 if key == '_id':
                     curr.update({'_id':val})
                 else:
@@ -89,5 +110,11 @@ class MongoDatabase:
                     decrypted_val = fernet.decrypt(bytes(val, "utf-8")).decode()
                     curr.update({decrypted_key:json.loads(decrypted_val)})
             entries.append(curr)
-            i += 1
+        cursor.close()
         return entries
+    
+    def num_docs(self, table):
+        return self.mongo_db[table].count_documents({})
+
+    def objectid_to_id(self, objectid):
+        return int(str(objectid), 16)
